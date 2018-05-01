ab4ed2204d58ec973bbe5f4a9c01d3d9c7c5b4b6
Rui WANG <reneewang1114@gmail.com>
2018-05-01 22:29:58 +0100

    Basically finish the first page to send page js However, the content is not right which needs correction.

--- pages/index/index.js
+++ pages/index/index.js
@@ -18,10 +18,11 @@ Page({
       },
       success: res => {
         let articles = res.data.result
-        let firstNews = [articles[0].title, (articles[0].source === "") ? "来源不明" : articles[0].source, articles[0].date.substring(11, 16),(articles[0].firstImage === "") ? "images/default-news.jpg" : articles[0].firstImage]
+        let firstNews = [articles[0].id,articles[0].title, (articles[0].source === "") ? "来源不明" : articles[0].source, articles[0].date.substring(11, 16),(articles[0].firstImage === "") ? "images/default-news.jpg" : articles[0].firstImage]
         let newsList = []
         for (let i = 1; i < articles.length; i += 1) {
           newsList.push({
+            id: articles[i].id,
             title: articles[i].title,
             source: (articles[i].source === "") ? "来源不明" : articles[i].source,
             date:articles[i].date.substring(11,16),
@@ -34,5 +35,19 @@ Page({
         })
       }
     })
-  }
+  },
+  //tap the first news
+  onTapFirstNews() {
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + this.data.firstNews[0],
+    })
+  },
+  //tap other news
+  onTapNewsList(event) {
+    console.log(event)
+    let index = event.currentTarget.id
+    wx.navigateTo({
+      url: '/pages/detail/detail?id=' + index,
+    })
+  },
 })
