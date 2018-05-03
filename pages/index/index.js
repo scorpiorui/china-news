8d5ff6cf9cfe6982484a127f282e855960752bdf
Rui WANG <reneewang1114@gmail.com>
2018-05-03 11:27:40 +0100

    Modified following the instructor comments. 1. 下拉刷新更新（待检查） 2. 如果Articles是空数组的处理 3. 合并onTapFirstNews和onTapNewsList函数 4. 列表渲染新闻类别（之前已完成） 5. 修改逻辑表达式及优化冗余
    还有两个问题：hover及阅读数增加

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,21 +1,28 @@
 //index.js
 
 Page({
-  //如果第一条新闻可以用nth-child进行布局则将相关函数和数据合并。
+  
   data: {
     navList: [{ id: "gn", text: "国内" }, { id: "gj", text: "国际" }, { id: "cj", text: "财经" }, { id: "yl", text: "娱乐" }, { id: "js", text: "军事" }, { id: "ty", text: "体育" }, { id: "other", text: "其他" }],//wx:for新闻分类
-    firstNews:[],//将第一则新闻的元素放入firstnews
+    defaultNewsType:"gn",
+    firstNewsId:'',
+    firstNewsTitle:'',
+    firstNewsSource:'',
+    firstNewsTime:'',
+    firstNewsImage:'',
     newsList: []//将其他新闻的元素放入newsList
   },
   onLoad() {
     this.showNews()
   },
   onPullDownRefresh: function () {
-    wx.stopPullDownRefresh()
+    this.showNews(() => {
+      wx.stopPullDownRefresh()
+    })
   },
   //showNews根据点击的新闻类别动态显示新闻，默认为国内新闻。
-  showNews(event) {
-    let newsType = event ? event.currentTarget.id :"gn"//default news:gn
+  showNews(event,callback) {
+    let newsType = event ? event.currentTarget.id :this.data.defaultNewsType
     let newsUrl = 'https://test-miniprogram.com/api/news/list'
     wx.request({
       url: newsUrl,
@@ -24,38 +31,48 @@ Page({
       },
       success: res => {
         let articles = res.data.result
-        //firstNews为第一则新闻的标题、来源、时间、图片的集合，其中来源和图片为空则显示默认值，时间只显示hh:mm.
-        let firstNews = [articles[0].id,articles[0].title, (articles[0].source === "") ? "来源不明" : articles[0].source, articles[0].date.substring(11, 16),(articles[0].firstImage === "") ? "images/default-news.jpg" : articles[0].firstImage]
-        //newsList为其他新闻的标题、来源、时间、图片的集合，其中来源和图片为空则显示默认值，时间只显示hh:mm.
-        let newsList = []
-        for (let i = 1; i < articles.length; i += 1) {
-          newsList.push({
-            id: articles[i].id,
-            title: articles[i].title,
-            source: (articles[i].source === "") ? "来源不明" : articles[i].source,
-            date:articles[i].date.substring(11,16),
-            image: (articles[i].firstImage === "") ? "images/default-news.jpg" : articles[i].firstImage
+        if(articles && articles!=[]){
+          let firstNewsId = articles[0].id
+          let firstNewsTitle = articles[0].title
+          let firstNewsSource = !articles[0].source ? "来源不明" : articles[0].source
+          let firstNewsTime = articles[0].date.substring(11, 16)
+          let firstNewsImage = !articles[0].firstImage ? "images/default-news.jpg" : articles[0].firstImage
+
+          //newsList为其他新闻的标题、来源、时间、图片的集合，其中来源和图片为空则显示默认值，时间只显示hh:mm.
+          let newsList = []
+          for (let i = 1; i < articles.length; i += 1) {
+            newsList.push({
+              id: articles[i].id,
+              title: articles[i].title,
+              source: !articles[i].source ? "来源不明" : articles[i].source,
+              date: articles[i].date.substring(11, 16),
+              image: !articles[i].firstImage ? "images/default-news.jpg" : articles[i].firstImage
+            })
+          }
+          this.setData({
+            newsList: newsList,
+            firstNewsId: firstNewsId,
+            firstNewsTitle: firstNewsTitle,
+            firstNewsSource: firstNewsSource,
+            firstNewsTime: firstNewsTime,
+            firstNewsImage: firstNewsImage
+          })
+        }else{
+          wx.showToast({
+            title: '没有新闻了。',
           })
         }
-        this.setData({
-          newsList: newsList,
-          firstNews:firstNews
-        })
+      },
+      complete: () => {
+        callback && callback()
       }
     })
   },
   //点击第一条新闻调用onTapFirstNews() 
-  onTapFirstNews() {
-    wx.navigateTo({
-      url: '/pages/detail/detail?id=' + this.data.firstNews[0],
-    })
-  },
-  //点击其他新闻调用onTapNewsList(event)
-  onTapNewsList(event) {
-    console.log(event)
-    let index = event.currentTarget.id
+  onTapNews(event) {
+    let index = !event.currentTarget.id ? this.data.firstNewsId : event.currentTarget.id
     wx.navigateTo({
       url: '/pages/detail/detail?id=' + index,
     })
-  },
+  }
 })
